4c129d99b81a197818e0d6de013e28b18b091989
topben <ben@tokimi.space>
2022-07-19 01:41:54 +0800

    feat: 🎸 add swagger

--- main.go
+++ main.go
@@ -13,6 +13,22 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// @title Ifandonlyif API
+// @version 1.0
+// @description This is a sample server server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:1323
+// @BasePath /
+// @schemes http
+
 func main() {
 	config, err := util.LoadConfig(".")
 	if err != nil {
