b02a7f44513c4d565c642dcbb3147b33ea9cade3
topben <ben@tokimi.space>
2022-09-09 21:49:20 +0800

    fix: 🐛 remove '' of NOW()

--- db/migration/000004_add_gas_prices.up.sql
+++ db/migration/000004_add_gas_prices.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "gas_prices" (
   "id" uuid DEFAULT gen_random_uuid() PRIMARY KEY,
   "average" integer,
-  "created_at" timestamptz DEFAULT 'now()'
+  "created_at" timestamptz DEFAULT now()
 );
