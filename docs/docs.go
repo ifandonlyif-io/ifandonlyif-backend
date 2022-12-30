e4735b011890c90fa29e33addae4ebd66c8893ae
Shane Zhou <s5355049@gmail.com>
2022-12-30 12:31:05 +0800

    Update docs

--- docs/docs.go
+++ docs/docs.go
@@ -571,7 +571,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:1323",
+	Host:             "",
 	BasePath:         "/",
 	Schemes:          []string{"http"},
 	Title:            "Ifandonlyif API",
--- main.go
+++ main.go
@@ -25,7 +25,6 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host localhost:1323
 // @BasePath /
 // @schemes http
 
