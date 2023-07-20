ee6622b1d35577d70d50e9e8488c98a530d3784d
topben <ben@tokimi.space>
2023-07-20 18:49:24 +0800

    refactor: 💡 remove unused codes

--- api/nft.go
+++ api/nft.go
@@ -2,38 +2,9 @@ package api
 
 import (
 	"net/http"
-	"strconv"
-	"time"
 
-	db "github.com/ifandonlyif-io/ifandonlyif-backend/db/sqlc"
 	"github.com/labstack/echo/v4"
 )
-
-func (server *Server) getNFTs(c echo.Context) error {
-	nfts, err := server.store.ListIffNfts(c.Request().Context())
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err)
-	}
-	return c.JSON(http.StatusOK, nfts)
-}
-
-func (server Server) createNFT(e echo.Context) error {
-	projectId := e.FormValue("projectId")
-	parseInt, _ := strconv.ParseInt(projectId, 10, 64)
-
-	created, err := server.store.CreateIffNft(e.Request().Context(), db.CreateIffNftParams{
-		ProjectID:                  parseInt,
-		UserWalletAddress:          e.FormValue("wallet"),
-		NftProjectsContractAddress: e.FormValue("projectContractAddress"),
-		NftProjectsCollectionName:  e.FormValue("collectionName"),
-		MintDate:                   time.Now(),
-		MintTransaction:            e.FormValue("mintTransaction"),
-	})
-	if err != nil {
-		return e.JSON(http.StatusInternalServerError, err)
-	}
-	return e.JSON(http.StatusCreated, created)
-}
 
 // nft godoc
 // @Summary      nftProjects
